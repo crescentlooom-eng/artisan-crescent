6a3dacb368126ff3f1720f7459d084621f5611f0
crescentlooom-eng <crescent.looom@gmail.com>
2026-06-09 03:33:26 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -11,7 +11,7 @@ export default function HomePage() {
   const [products, setProducts] = useState([]);
   const [featured, setFeatured] = useState([]);
 
-  useScrollReveal();
+  useScrollReveal([featured]);
 
   useEffect(() => {
     setProducts(listProducts({ new_arrival: true }));
