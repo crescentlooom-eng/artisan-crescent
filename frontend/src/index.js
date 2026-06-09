8ee452479a2da040f1bc2a08d33e61b952b5fcf6
crescentlooom-eng <crescent.looom@gmail.com>
2026-06-10 02:23:16 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -27,3 +27,8 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js');
   });
 }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
