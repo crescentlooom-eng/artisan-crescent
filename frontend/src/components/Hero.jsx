7416877b7021b1502becd9c0023f002251511101
crescentlooom-eng <crescent.looom@gmail.com>
2026-06-13 13:41:44 +0530

    Update Hero.jsx

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import WeaveHero from "@/components/WeaveHero";
+import MagneticHero from "@/components/MagneticHero";
 import MobileHero from "@/components/MobileHero";
 
 export default function Hero() {
   const isMobile = window.innerWidth < 768;
-  return isMobile ? <MobileHero /> : <WeaveHero />;
+  return isMobile ? <MobileHero /> : <MagneticHero />;
 }
